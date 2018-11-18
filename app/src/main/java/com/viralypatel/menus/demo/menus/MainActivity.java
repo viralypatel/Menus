dff9503a3ba66a77bc39cc47be820de519f39c75
Viral Patel <viral.y.patel@gmail.com>
2018-11-19 00:31:52 +0530

    upto m2 final

---
+++ app/src/main/java/com/viralypatel/menus/demo/menus/MainActivity.java
@@ -0,0 +1,145 @@
+package com.viralypatel.menus.demo.menus;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ActionMode;
+import android.support.v7.widget.PopupMenu;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+
+    ActionMode mActionMode;
+
+    private ActionMode.Callback mActionModeCallBack = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.actionmode_context_menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.actionmodemenu1:
+                    Toast.makeText(MainActivity.this, "Action Mode 1 Clicked", Toast.LENGTH_SHORT).show();
+                    mode.finish();
+                    return true;
+                case R.id.actionmodemenu2:
+                    Toast.makeText(MainActivity.this, "Action Mode 2 Clicked", Toast.LENGTH_SHORT).show();
+                    mode.finish();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            mActionMode = null;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView) findViewById(R.id.txtTitle);
+        this.registerForContextMenu(tv);
+
+        Button button = (Button) findViewById(R.id.btnActionModeMenu);
+        button.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                if(mActionMode != null) {
+                    return false;
+                }
+
+                mActionMode = startSupportActionMode(mActionModeCallBack);
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handling options menu item click
+        switch (item.getItemId()) {
+            case R.id.menuItem1:
+                Toast.makeText(this, "Menu Item 1 Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menuItem2:
+                Toast.makeText(this, "Menu Item 2 Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menuItem3:
+                Toast.makeText(this, "Menu Item 3 Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.floating_context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        // handle contextual floating menu item click
+        switch (item.getItemId()) {
+            case R.id.contextmenu1:
+                Toast.makeText(this, "Contextual Menu 1 Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.contextmenu2:
+                Toast.makeText(this, "Contextual Menu 2 Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    public void showPopupMenu(View view) {
+        PopupMenu popup = new PopupMenu(this, view);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.popup_menu);
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.popupmenu1:
+                Toast.makeText(this, "Popup Menu 1 Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.popupmenu2:
+                Toast.makeText(this, "Popup Menu 2 Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
+    }
+}
