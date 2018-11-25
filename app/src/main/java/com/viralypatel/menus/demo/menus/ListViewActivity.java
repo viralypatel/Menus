42560245814e9892a48a09231ae0124f80b91a3e
Viral Patel <viral.y.patel@gmail.com>
2018-11-26 01:35:04 +0530

    list view activity starting point

--- app/src/main/java/com/viralypatel/menus/demo/menus/ListViewActivity.java
+++ app/src/main/java/com/viralypatel/menus/demo/menus/ListViewActivity.java
@@ -2,7 +2,7 @@ package com.viralypatel.menus.demo.menus;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.SparseBooleanArray;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -13,16 +13,12 @@ public class ListViewActivity extends AppCompatActivity {
 
     private ListView mainListView ;
     private ArrayAdapter<String> listAdapter ;
+    ArrayList<String> planetList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
-
 
         // Find the ListView resource.
         mainListView = (ListView) findViewById( R.id.mainListView );
@@ -30,7 +26,7 @@ public class ListViewActivity extends AppCompatActivity {
         // Create and populate a List of planet names.
         String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
                 "Jupiter", "Saturn", "Uranus", "Neptune"};
-        ArrayList<String> planetList = new ArrayList<String>();
+        planetList = new ArrayList<String>();
         planetList.addAll( Arrays.asList(planets) );
 
         // Create ArrayAdapter using the planet list.
@@ -38,6 +34,24 @@ public class ListViewActivity extends AppCompatActivity {
 
         // Set the ArrayAdapter as the ListView's adapter.
         mainListView.setAdapter( listAdapter );
+
+
+
+    }
+
+    // delete selected items from list view
+    private void deleteSelectedItems() {
+        /** Getting the checked items from the listview */
+        SparseBooleanArray checkedItemPositions = mainListView.getCheckedItemPositions();
+        int itemCount = mainListView.getCount();
+
+        for (int i = itemCount - 1; i >= 0; i--) {
+            if (checkedItemPositions.get(i)) {
+                listAdapter.remove(planetList.get(i));
+            }
+        }
+        checkedItemPositions.clear();
+        listAdapter.notifyDataSetChanged();
     }
 
 }
